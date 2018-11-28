8072f6b1180ed24bce43b4af754ec77e8817d760
TimHunter <thetimmeh@hotmail.com>
2018-11-27 22:14:33 -0600

    Added NavBar and CSS to Login, Built Working ViewOrders Page
    ViewOrders can now show off all the Cart Orders associated to the logged in user.

--- ViewOrders.php
+++ ViewOrders.php
@@ -1,33 +1,81 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <?php 
+            require 'verification.php';
+        ?>
         <link rel="stylesheet" type="text/css" href="Main.css" >
     </head>
     
     <body>
-        <?php include 'NavBar.html' ?>
+        <?php 
+            include 'NavBar.html';
+        ?>
         
         <div id="body-content">
-            
-            <?php echo 'Hello!'; ?>
 
             <?php
-                $data = array(
-                    array("Row1", "Item", "Item"), 
-                    array("Row2", "Item", "Item"), 
-                    array("Row3", "Item", "Item")
-                );
+            
+                $cartId = "";
+                $cartCost = "";
+                $cartNumber = "";
+                $cartArray = [];
+                $username = $_SESSION['username'];
+            
+                $conn = oci_connect('sizheng', 'Dec371996', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db2.ndsu.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
 
-                echo '<table>';
-                echo '<tr><th>Col1</th><th>Col2</th><th>Col3</th></tr>';
-                foreach($data as $row){
-                    echo '<tr>';
-                    foreach($row as $col){
-                        echo '<td>'.$col.'</td>';
-                    }
-                    echo '</tr>';
+                $cartQuery = "SELECT co.ID, co.TOTAL_COST, co.TOTAL_NUMBER FROM USER_T u INNER JOIN CART_ORDER co ON u.USERNAME = '$username'";
+                $stid = oci_parse($conn, $cartQuery);
+            
+                oci_define_by_name($stid, 'ID', $cartId);
+                oci_define_by_name($stid, 'TOTAL_COST', $cartCost);
+                oci_define_by_name($stid, 'TOTAL_NUMBER', $cartNumber);
+
+                oci_execute($stid,OCI_DEFAULT);
+
+                while($row = oci_fetch_array($stid, OCI_ASSOC))
+                {
+                    array_push($cartArray, array($cartId, $cartCost, $cartNumber));
                 }
-                echo '</table>';
+            
+                oci_free_statement($stid);
+                
+                foreach($cartArray as $cartRow) {
+                    echo "<div style='display:flex;'>";
+                        echo "<div style='flex: 1; border: solid black 1px; border-radius: 10px; padding: 10px; height: 90px; margin-right: 10px; background-color: #EFEFFF;'>";
+                            echo "<br/><b>ORDER ID:</b> $cartRow[0]<br/>";
+                            echo "<b>TOTAL COST:</b> $$cartRow[1]<br/>";
+                            echo "<b>TOTAL ITEMS:</b> $cartRow[2]<br/><br/>";
+                        echo "</div>";
+
+                        $itemQuery = "SELECT i.NAME, i.PRICE, i.CATEGORY, ci.NUMBER_OF_ITEM FROM CART_ITEM ci INNER JOIN ITEM i ON ci.ORDER_ID = '$cartRow[0]' AND ci.ITEM_ID = i.ID";
+
+                        $stid = oci_parse($conn, $itemQuery);
+
+                        oci_execute($stid,OCI_DEFAULT);
+                        
+                        echo "<div style='flex: 4;'>";
+                            echo '<table>';
+                                echo '<tr><th>NAME</th><th>PRICE</th><th>CATEGORY</th><th>NUMBER OF ITEM</th></tr>';
+                                while($row = oci_fetch_array($stid, OCI_ASSOC))
+                                {
+                                    echo '<tr>';
+                                    foreach ($row as $item)    
+                                    {
+                                        echo '<td>'.$item.'</td>';
+                                    }
+                                    echo '</tr>';
+                                }
+                            echo '</table>';
+                        echo "</div>";
+                    echo "</div><br/><br/>";
+                    
+                    oci_free_statement($stid);
+                }
+
+                oci_close($conn);
+            
+                //echo "<script type='text/javascript'>alert('$query');</script>";
             ?>
             
         </div>
--- login1.php
+++ login1.php
@@ -1,110 +1,107 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<style>
-.error {color: #FF0000;}
-</style>
+    <style>
+    .error {color: #FF0000;}
+    </style>
+    <link rel="stylesheet" type="text/css" href="Main.css" >
 </head>
 <body>
 
-<?php
-// define variables and set to empty values
-session_start();
-$nameErr = $passErr = "";
-$name = $pass = $comment = $password = "";
+    <?php 
+        include 'NavBar.html';
+    ?>
+    
+    <?php
+        // define variables and set to empty values
+        session_start();
+        $nameErr = $passErr = "";
+        $name = $pass = $comment = $password = "";
+        $userId = 0;
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["name"])) {
-    $nameErr = "Name is required";
-  } else {
-    $name = test_input($_POST["name"]);
-    // check if name only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
-      $nameErr = "Only letters and white space allowed";
-    }
-  }
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+          if (empty($_POST["name"])) {
+            $nameErr = "Name is required";
+          } else {
+            $name = test_input($_POST["name"]);
+            // check if name only contains letters and whitespace
+            if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+              $nameErr = "Only letters and white space allowed";
+            }
+          }
 
-  if (empty($_POST["pass"])) {
-    $passErr = " password is required";
-  } else {
-    $pass = test_input($_POST["pass"]);
-  }
-
-
-  // if (empty($_POST["comment"])) {
-  //   $comment = "";
-  // } else {
-  //   $comment = test_input($_POST["comment"]);
-  // }
+          if (empty($_POST["pass"])) {
+            $passErr = " password is required";
+          } else {
+            $pass = test_input($_POST["pass"]);
+          }
 
 
-}
+        }
 
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
-?>
+        function test_input($data) {
+          $data = trim($data);
+          $data = stripslashes($data);
+          $data = htmlspecialchars($data);
+          return $data;
+        }
+    ?>
 
-<h2>login</h2>
-<p><span class="error">* required field</span></p>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-  Name: <input type="text" name="name" value="<?php echo $name;?>">
-  <span class="error">* <?php echo $nameErr;?></span>
-  <br><br>
+    <h2>login</h2>
+    <p><span class="error">* required field</span></p>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+      Name: <input type="text" name="name" value="<?php echo $name;?>">
+      <span class="error">* <?php echo $nameErr;?></span>
+      <br><br>
 
-  Password: <input type="text" name="pass" value="<?php echo $pass;?>">
-  <br><br>
-  <!-- Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
-  <br><br> -->
-  <input type="submit" name="submit" value="Submit">
-</form>
+      Password: <input type="text" name="pass" value="<?php echo $pass;?>">
+      <br><br>
+      <!-- Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+      <br><br> -->
+      <input type="submit" name="submit" value="Submit">
+    </form>
 
-<?php
-echo "<h2>Your Input:</h2>";
-echo $name;
-echo "<br>";
-echo $pass;
+    <?php
+        echo "<h2>Your Input:</h2>";
+        echo $name;
+        echo "<br>";
+        echo $pass;
+    ?>
 
+    <?php
+        // Remember to replace 'username' and 'password'!
+        $conn = oci_connect('sizheng', 'Dec371996', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db2.ndsu.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
 
-?>
+        //put your query here
+        $query = "SELECT * FROM USER_T where USERNAME = '$name'";
+        $stid = oci_parse($conn,$query);
 
-<?php
-// Remember to replace 'username' and 'password'!
-$conn = oci_connect('sizheng', 'Dec371996', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db2.ndsu.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
+        oci_define_by_name($stid, 'PASSWORD', $password);
 
-//put your query here
-$query = "SELECT * FROM USER_T where USERNAME = '$name'";
-$stid = oci_parse($conn,$query);
+        oci_execute($stid,OCI_DEFAULT);
 
-oci_define_by_name($stid, 'PASSWORD', $password);
+        //iterate through each row
+        //while ($row = oci_fetch_array($stid,OCI_ASSOC))
+        while(oci_fetch($stid))
+        {
+            //iterate through each item in the row and echo it
+            if($password == $pass){
+                //foreach ($row as $item)
+                //{
 
-oci_execute($stid,OCI_DEFAULT);
+                    echo "WELCOME $name";
+                    echo $password;
 
-//iterate through each row
-//while ($row = oci_fetch_array($stid,OCI_ASSOC))
-while(oci_fetch($stid))
-{
-    //iterate through each item in the row and echo it
-    if($password == $pass){
-        //foreach ($row as $item)
-        //{
+                    $_SESSION['username'] = $name;
+                    $_SESSION['loggedin'] = true;
+                //}
+              }
 
-            echo "WELCOME $name";
-            echo $password;
-
-            $_SESSION['username'] = $name;
-            $_SESSION['loggedin'] = true;
-        //}
-      }
-
-  echo '<br/>';
-}
-oci_free_statement($stid);
-oci_close($conn);
-?>
+          echo '<br/>';
+        }
+        oci_free_statement($stid);
+        oci_close($conn);
+    ?>
 
 
 </body>
