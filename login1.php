87e3707598dbd3c3194ff5a3d266198903a701fc
TimHunter <thetimmeh@hotmail.com>
2018-11-28 19:17:24 -0600

    Changed Login Styling
    CHanged the look of the login page. Flipped two NavBar menus.

--- login1.php
+++ login1.php
@@ -2,7 +2,28 @@
 <html>
 <head>
     <style>
-    .error {color: #FF0000;}
+        #center-login {
+            width: 300px;
+            margin: 0 auto;
+        }
+        h2, h4 {
+            text-align: center;
+        }
+        .form-element {
+            display: flex;
+        }
+        .form-element label {
+            padding-right: 10px;
+            margin-bottom: 15px;
+        }
+        .form-element input {
+            height: 15px;
+            flex: 1;
+        }
+        #submit {
+            margin: 1em 5em;
+            text-align: center;
+        }
     </style>
     <link rel="stylesheet" type="text/css" href="Main.css" >
 </head>
@@ -46,62 +67,63 @@
           return $data;
         }
     ?>
+    
+    <div id="body-content">
+        <div id="center-login">
+            <h2>Login</h2>
+            
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                <div class="form-element">
+                    <label for="name">Name: </label>
+                    <input id="name" type="text" name="name" value="<?php echo $name;?>">
+                </div>
+                <div class="form-element">
+                    <label for="pass">Password: </label>
+                    <input id="pass" type="text" name="pass" value="<?php echo $pass;?>">
+                </div>
+                <div class="form-element">
+                    <input id="submit" type="submit" name="submit" value="Submit" style="height: 30px;">
+                </div>
+            </form>
 
-    <h2>login</h2>
-    <p><span class="error">* required field</span></p>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-      Name: <input type="text" name="name" value="<?php echo $name;?>">
-      <span class="error">* <?php echo $nameErr;?></span>
-      <br><br>
 
-      Password: <input type="text" name="pass" value="<?php echo $pass;?>">
-      <br><br>
-      <!-- Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
-      <br><br> -->
-      <input type="submit" name="submit" value="Submit">
-    </form>
 
-    <?php
-        echo "<h2>Your Input:</h2>";
-        echo $name;
-        echo "<br>";
-        echo $pass;
-    ?>
+            <?php
+                // Remember to replace 'username' and 'password'!
+                $conn = oci_connect('sizheng', 'Dec371996', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db2.ndsu.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
 
-    <?php
-        // Remember to replace 'username' and 'password'!
-        $conn = oci_connect('sizheng', 'Dec371996', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db2.ndsu.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
+                //put your query here
+                $query = "SELECT * FROM USER_T where USERNAME = '$name'";
+                $stid = oci_parse($conn,$query);
 
-        //put your query here
-        $query = "SELECT * FROM USER_T where USERNAME = '$name'";
-        $stid = oci_parse($conn,$query);
+                oci_define_by_name($stid, 'PASSWORD', $password);
 
-        oci_define_by_name($stid, 'PASSWORD', $password);
+                oci_execute($stid,OCI_DEFAULT);
 
-        oci_execute($stid,OCI_DEFAULT);
+                //iterate through each row
+                //while ($row = oci_fetch_array($stid,OCI_ASSOC))
+                while(oci_fetch($stid))
+                {
+                    //iterate through each item in the row and echo it
+                    if($password == $pass){
+                        //foreach ($row as $item)
+                        //{
 
-        //iterate through each row
-        //while ($row = oci_fetch_array($stid,OCI_ASSOC))
-        while(oci_fetch($stid))
-        {
-            //iterate through each item in the row and echo it
-            if($password == $pass){
-                //foreach ($row as $item)
-                //{
+                            echo "<h4>Welcome $name!</h4>";
 
-                    echo "WELCOME $name";
-                    echo $password;
+                            $_SESSION['username'] = $name;
+                            $_SESSION['loggedin'] = true;
+                        //}
+                      }
 
-                    $_SESSION['username'] = $name;
-                    $_SESSION['loggedin'] = true;
-                //}
-              }
-
-          echo '<br/>';
-        }
-        oci_free_statement($stid);
-        oci_close($conn);
-    ?>
+                  echo '<br/>';
+                }
+                oci_free_statement($stid);
+                oci_close($conn);
+            ?>
+        </div>
+        
+    </div>
 
 
 </body>
